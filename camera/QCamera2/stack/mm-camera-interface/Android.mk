f1dd5fe974a4e1b094546d31579c489ceadab498
Yifan Hong <elsk@google.com>
2017-06-27 11:14:25 -0700

    land: Fix dependencies for mm-camera-interface
    Test: mma -j BOARD_VNDK_VERSION=current
    Bug: 33241851
    Change-Id: I603e86e78c80e592e852da184ca021fe42a8ea9a

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -56,6 +56,7 @@ LOCAL_MODULE           := libmmcamera_interface
 LOCAL_CLANG := false
 LOCAL_PRELINK_MODULE   := false
 LOCAL_SHARED_LIBRARIES := libdl libcutils liblog libutils
+LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_HEADER_LIBRARIES += generated_kernel_headers
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true

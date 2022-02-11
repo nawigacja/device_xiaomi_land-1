c32c7b6d9ebe4d1cdf9b3de784f7119c8a3734c9
Magicxavi <rolfpeterkesche@gmail.com>
2022-02-11 14:06:20 +0400

    land: Copy custom apns-conf
    E/TelephonyProvider(2846): FileNotFoundException for /system/etc/apns-conf.xml:java.io.FileNotFoundException: /system/etc/apns-conf.xml: open failed: ENOENT (No such file or directory)
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- device.mk
+++ device.mk
@@ -68,6 +68,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \

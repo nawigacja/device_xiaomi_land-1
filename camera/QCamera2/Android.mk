203c0e10066fbbed0bf6d2f137b421cee4160660
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-16 14:53:04 +0800

    land: camera: Remove qcom-camera project maps

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -90,7 +90,7 @@ LOCAL_C_INCLUDES := \
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
-        $(call project-path-for,qcom-camera)/QCamera2/HAL
+        $(LOCAL_PATH)/QCamera2/HAL
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
